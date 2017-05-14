70871dc1594afe51caceecb0606ddb28c49d25f1
hubodashi <hubodashi@aliyun.com>
2017-05-15 07:13:31 +0800

    cannot add item with quantity 0

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -8,4 +8,19 @@ class CartItemsController < ApplicationController
     flash[:warning]="Deleted #{@product.title} from the cart"
     redirect_to :back
   end
+  def update
+    @cart=current_cart
+    @cart_item= @cart.cart_items.find_by(product_id: params[:id])
+    if @cart_item.product.quantity >= cart_item_params[:quantity].to_i
+    @cart_item.update(cart_item_params)
+    flash[:notice]="Items updated Success"
+    else
+    flash[:warning]="No enough products"
+    end 
+    redirect_to carts_path
+  end
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:quantity)
+  end
 end
